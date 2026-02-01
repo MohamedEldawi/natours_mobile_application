5f23cf402df71169dfd6569844746374f3c3868a
Mohamed Hesham <mohamed.eldawi@ejust.edu.eg>
2026-02-01 23:24:21 +0200

    - added development & production flavors

---
+++ lib/main_development.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:natours_application/core/Routing/app_router.dart';
+import 'package:natours_application/core/di/dependency_injection.dart';
+import 'package:natours_application/natours_app.dart';
+
+void main() async {
+  await ScreenUtil.ensureScreenSize(); // to fix disappearing text with screen util
+  setupGetIt();
+  runApp(NatoursApp(router: AppRouter()));
+}
