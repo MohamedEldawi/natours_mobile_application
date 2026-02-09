e4cf3f86208094026fcb4ed804816a12013ad0b9
Mohamed Hesham <mohamed.eldawi@ejust.edu.eg>
2026-02-09 16:50:06 +0200

    - secured user token

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -28,7 +28,10 @@ void main() async {
 }
 
 Future<void> checkIfUserLoggedIn() async {
-  String userToken = await SharedPrefHelper.getString(SharedPrefKeys.userToken);
+  // await SharedPrefHelper.clearAllSecureDate();
+  String userToken = await SharedPrefHelper.getSecureString(
+    SharedPrefKeys.userToken,
+  );
   if (userToken.isNotEmpty && !(JwtDecoder.isExpired(userToken))) {
     isLoggedIn = true;
   } else {
