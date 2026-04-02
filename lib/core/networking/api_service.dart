222dcc743ca39aedbb93365782e9fe30779dac47
Mohamed Hesham <mohamed.eldawi@ejust.edu.eg>
2026-04-02 14:42:45 +0200

    changed the base url

--- lib/core/networking/api_service.dart
+++ lib/core/networking/api_service.dart
@@ -9,7 +9,7 @@ import 'package:natours_application/core/networking/api_constants.dart';
 import 'package:retrofit/retrofit.dart';
 part 'api_service.g.dart';
 
-@RestApi(baseUrl: ApiConstants.baseUrl)
+@RestApi(baseUrl: ApiConstants.apiUrl)
 abstract class ApiService {
   factory ApiService(Dio dio, {String? baseUrl}) = _ApiService;
   @POST(ApiConstants.login)
